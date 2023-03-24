499579d8114e41d399d9826ce3f50eaa125c8107
jeffthechristian <daiiviids@gmail.com>
2023-03-24 18:30:24 +0200

    just doing sutff hoping it works lol

--- Alcholator/app/src/main/java/com/example/alcholator/HistoryActivity.java
+++ Alcholator/app/src/main/java/com/example/alcholator/HistoryActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
     }
 }
