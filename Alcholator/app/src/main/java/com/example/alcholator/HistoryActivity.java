c2258c2267b5e80b3da7069508b67fd24bdfd7aa
jeffthechristian <daiiviids@gmail.com>
2023-03-25 00:12:28 +0200

    WE DO IT

--- Alcholator/app/src/main/java/com/example/alcholator/HistoryActivity.java
+++ Alcholator/app/src/main/java/com/example/alcholator/HistoryActivity.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,82 +27,52 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
-
     private DatabaseReference mDatabase;
-    private ListView mListView;
-
-    ImageButton edit_profile, show_history, calculate;
+    private FirebaseUser currentUser;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        String currentUserUid = mAuth.getCurrentUser().getUid();
+        // Get the current user
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
+        // Get a reference to the Firebase Realtime Database
         mDatabase = FirebaseDatabase.getInstance().getReference("history2");
-        Query currentUserQuery = mDatabase.orderByChild("uid").equalTo(currentUserUid);
 
-        mListView = findViewById(R.id.listView);
+        listView = findViewById(R.id.listView);
 
-        edit_profile = findViewById(R.id.edit_profile);
-        show_history = findViewById(R.id.show_history);
-        calculate = findViewById(R.id.calculate);
-
-        ValueEventListener valueEventListener = new ValueEventListener() {
+        // Set up a listener for changes in the data
+        mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                ArrayList<String> historyEntries = new ArrayList<>();
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    String date = ds.child("date").getValue(String.class);
-                    String bloodResult = ds.child("bloodResult").getValue(String.class);
-                    String historyEntry = "Date: " + date + ", Blood Result: " + bloodResult;
-                    historyEntries.add(historyEntry);
+                // Create a new list to hold the data
+                ArrayList<String> listData = new ArrayList<>();
+
+                // Loop through the data and add it to the list if it belongs to the current user
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    String uid = data.getKey();
+                    if (uid.equals(currentUser.getUid())) {
+                        for (DataSnapshot child : data.getChildren()) {
+                            String bloodResult = child.child("bloodResult").getValue(String.class);
+                            String date = child.child("date").getValue(String.class);
+                            String dataString = "Blood result: " + bloodResult + ", Date: " + date;
+                            listData.add(dataString);
+                        }
+                    }
                 }
 
-                ArrayAdapter<String> adapter = new ArrayAdapter(
-                        HistoryActivity.this,
-                        android.R.layout.simple_list_item_1,
-                        historyEntries
-                ) {
-                    @NonNull
-                    @Override
-                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                        View view = super.getView(position, convertView, parent);
-                        TextView textView = view.findViewById(android.R.id.text1);
-                        textView.setTextColor(Color.WHITE);
-                        return view;
-                    }
-                };
-                mListView.setAdapter(adapter);
+                // Set up the adapter for the ListView
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(HistoryActivity.this, android.R.layout.simple_list_item_1, listData);
+                listView.setAdapter(adapter);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.d("HistoryActivity", databaseError.getMessage());
+                // Handle errors here
             }
-        };
-
-        currentUserQuery.addValueEventListener(valueEventListener);
-
-        edit_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HistoryActivity.this, DataInput.class));
-            }
-        } );
-        show_history.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HistoryActivity.this, HistoryActivity.class));
-            }
-        } );
-        calculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HistoryActivity.this, AlcoholCalculator.class));
-            }
-        } );
+        });
     }
 }
