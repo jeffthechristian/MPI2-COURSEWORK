a00c4397ab1e64ade5a95850f496f13295764294
Signe Grosberga <signe.grosberga@va.lv>
2023-03-25 02:23:07 +0200

    repaired some stuff

--- Alcholator/app/src/main/java/com/example/alcholator/HistoryActivity.java
+++ Alcholator/app/src/main/java/com/example/alcholator/HistoryActivity.java
@@ -1,10 +1,12 @@
 package com.example.alcholator;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
