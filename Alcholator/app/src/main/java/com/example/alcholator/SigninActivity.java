907fabe16e4ffcde59d19a436b8d4d91fcf5c6ad
jeffthechristian <daiiviids@gmail.com>
2023-03-26 02:38:28 +0200

    reversing

--- Alcholator/app/src/main/java/com/example/alcholator/SigninActivity.java
+++ Alcholator/app/src/main/java/com/example/alcholator/SigninActivity.java
@@ -26,13 +26,13 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SigninActivity extends AppCompatActivity {
-    public static String uid; // a static variable to hold the user ID after login
-    private EditText emailTextView, passwordTextView; // text fields for email and password input
-    private Button Btn; // button for login
-    private ProgressBar progressBar; // progress bar to indicate the login progress
-    public CheckBox rememberMe; // checkbox to remember the login credentials
+    public static String uid;
+    private EditText emailTextView, passwordTextView;
+    private Button Btn;
+    private ProgressBar progressBar;
+    public CheckBox rememberMe;
 
-    private FirebaseAuth mAuth; // Firebase authentication object
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +41,10 @@ public class SigninActivity extends AppCompatActivity {
 
         rememberMe = findViewById(R.id.rememberme);
 
-        // get an instance of the Firebase authentication object
+        // taking instance of FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
 
-        // initialize all the views through their IDs defined above
+        // initialising all views through id defined above
         emailTextView = findViewById(R.id.email);
         passwordTextView = findViewById(R.id.password);
         Btn = findViewById(R.id.login);
@@ -57,75 +57,86 @@ public class SigninActivity extends AppCompatActivity {
 
         if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
             // Automatically log in the user using saved login credentials
-            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        // User logged in successfully
-                        uid = mAuth.getCurrentUser().getUid();
-                        Toast.makeText(getApplicationContext(), "Login successful!!", Toast.LENGTH_LONG).show();
-                        // if sign-in is successful, start the AlcoholCalculator activity
-                        Intent intent = new Intent(SigninActivity.this, AlcoholCalculator.class);
-                        startActivity(intent);
-                        finish(); // close the SigninActivity to prevent going back to it with the back button
-                    } else {
-                        // User login failed
-                        Toast.makeText(getApplicationContext(), "Login failed!!", Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // User logged in successfully
+                                uid = mAuth.getCurrentUser().getUid();
+                                Toast.makeText(getApplicationContext(),
+                                                "Login successful!!",
+                                                Toast.LENGTH_LONG)
+                                        .show();
+                                // if sign-in is successful
+                                Intent intent = new Intent(SigninActivity.this, AlcoholCalculator.class);
+                                startActivity(intent);
+                                finish(); // close the SigninActivity
+                            } else {
+                                // User login failed
+                                Toast.makeText(getApplicationContext(),
+                                                "Login failed!!",
+                                                Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+                    });
 
-            // Set an onClickListener on the Sign-in button to call loginUserAccount() when clicked
-            Btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    loginUserAccount();
-                }
-            });
+        } // Set on Click Listener on Sign-in button
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUserAccount();
+            }
+        });
 
-            // Set up the "Not a member? Sign up now!" clickable text
-            TextView signupTextView = findViewById(R.id.signup_textview);
-            String text = "Not a member? Sign up now!";
-            SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(text);
-            ClickableSpan clickableSpan = new ClickableSpan() {
-                @Override
-                public void onClick(View view) {
-                    // When the text is clicked, start the SignupActivity
-                    Intent intent = new Intent(SigninActivity.this, SignupActivity.class);
-                    startActivity(intent);
-                }
-            };
-            // Add the clickable span to the "Sign up now!" text
-            spannableStringBuilder.setSpan(clickableSpan, text.indexOf("Sign up now!"), text.indexOf("Sign up now!") + "Sign up now!".length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            signupTextView.setText(spannableStringBuilder);
-            signupTextView.setMovementMethod(LinkMovementMethod.getInstance());
-        }
+        TextView signupTextView = findViewById(R.id.signup_textview);
+        String text = "Not a member? Sign up now!";
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(text);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SigninActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        };
+        spannableStringBuilder.setSpan(clickableSpan, text.indexOf("Sign up now!"), text.indexOf("Sign up now!") + "Sign up now!".length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        signupTextView.setText(spannableStringBuilder);
+        signupTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
-    // This method is called when the user clicks the Sign-in button
     private void loginUserAccount() {
 
-        // Show the progress bar to indicate that the login is being processed
+        // show the visibility of progress bar to show loading
         progressBar.setVisibility(View.VISIBLE);
 
-        // Get the email and password entered by the user
+        // Take the value of two edit texts in Strings
         String email, password;
         email = emailTextView.getText().toString();
         password = passwordTextView.getText().toString();
 
-        // Perform some basic validation on the email and password
+        // validations for input email and password
         if (TextUtils.isEmpty(email)) {
-            Toast.makeText(getApplicationContext(), "Please enter email!!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_SHORT)
+                    .show();
             return;
         }
 
         if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getApplicationContext(), "Please enter password!!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_SHORT)
+                    .show();
             return;
         }
 
         if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            Toast.makeText(getApplicationContext(), "Invalid email or password!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),
+                            "Invalid email or password!",
+                            Toast.LENGTH_SHORT)
+                    .show();
             return;
         }
 
@@ -139,31 +150,43 @@ public class SigninActivity extends AppCompatActivity {
             editor.apply();
         }
 
-        // Sign-in an existing user with the provided email and password
+        // signin existing user
         mAuth.signInWithEmailAndPassword(email, password)
-                // Add a listener to be notified when the authentication attempt is complete
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // If sign-in is successful, get the user ID and show a success message
-                            uid = mAuth.getCurrentUser().getUid();
-                            Toast.makeText(getApplicationContext(), "Login successful!!", Toast.LENGTH_LONG).show();
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
 
-                            // Hide the progress bar
-                            progressBar.setVisibility(View.GONE);
+                                    uid = mAuth.getCurrentUser().getUid();
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login successful!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
 
-                            // Create an intent to navigate to the home activity
-                            Intent intent = new Intent(SigninActivity.this, AlcoholCalculator.class);
-                            startActivity(intent);
-                        } else {
-                            // If sign-in fails, show an error message
-                            Toast.makeText(getApplicationContext(), "Login failed!!", Toast.LENGTH_LONG).show();
+                                    // hide the progress bar
+                                    progressBar.setVisibility(View.GONE);
 
-                            // Hide the progress bar
-                            progressBar.setVisibility(View.GONE);
-                        }
-                    }
-                });
+                                    // if sign-in is successful
+                                    // intent to home activity
+                                    Intent intent
+                                            = new Intent(SigninActivity.this,
+                                            AlcoholCalculator.class);
+                                    startActivity(intent);
+                                } else {
+
+                                    // sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login failed!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // hide the progress bar
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            }
+                        });
     }
+
 }
